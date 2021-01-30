463f91f5a1de35475bcea24bd0295f6b71202f9d
varunkanu <76861446+varunkanu@users.noreply.github.com>
2021-01-30 11:42:27 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+const button = document.getElementById("button");
+const red = document.getElementById("red");
+const redImage = document.getElementById("red-image");
+const blueImage = document.getElementById("blue-image");
+const yellowImage = document.getElementById("yellow-image");
+
+button.onclick = () => {
+if (red.checked) {
+redImage.classList.remove("hidden");
+redImage.classList.add("eject");
+}
+if (blue.checked) {
+blueImage.classList.remove("hidden");
+blueImage.classList.add("eject");
+}
+if (yellow.checked) {
+yellowImage.classList.remove("hidden");
+yellowImage.classList.add("eject");
+}
+};
+
